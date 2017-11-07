97a24997daed659b6374ee6332d62265cdb0d1ac
Agustin Moya <agustin.moya@despegar.com>
2017-11-07 19:22:46 -0300

    Cambios en tabla de destinos

--- tp-algoritmos2/src/com/company/tpIntegrador/TablaDestinoInfo/TablaDeDestinos.java
+++ tp-algoritmos2/src/com/company/tpIntegrador/TablaDestinoInfo/TablaDeDestinos.java
@@ -1,5 +1,19 @@
 package com.company.tpIntegrador.TablaDestinoInfo;
 
+import java.util.List;
+
 public class TablaDeDestinos {
-    //Preguntar como representar la tabla
+    private List<RegistroDestino> destinos;
+
+    public TablaDeDestinos(List<RegistroDestino> destinos) {
+        this.destinos = destinos;
+    }
+
+    public List<RegistroDestino> getDestinos() {
+        return destinos;
+    }
+
+    public void setDestinos(List<RegistroDestino> destinos) {
+        this.destinos = destinos;
+    }
 }
